e8b1f7b8ed9397f928540d4e54f22784d677d2ec
Moussa FALL <moussafall005@gmail.com>
2019-11-30 23:39:46 +0100

    ajout des vues

---
+++ src/main/java/sen/school/primaire/Interface/InterfaceController.java
@@ -0,0 +1,44 @@
+package sen.school.primaire.Interface;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InterfaceController {
+
+    //creer annee scolaire
+    @GetMapping("/annee/create")
+    public String creerAnneeScolaire(){
+        return "addAnnee";
+    }
+
+    // creer une école
+    @GetMapping("/ecole/create")
+    public String creerEcole(){
+        return "addEcole";
+    }
+
+    //ajouter une classe dans une école pour une année scolaire donnée
+    @GetMapping("/classe/create")
+    public String addClasse(){
+        return "addClasse";
+    }
+
+    //creer une matiere
+    @GetMapping("/matiere/create")
+    public String addMatiere(){
+        return "addMatiere";
+    }
+
+    //affecter un coefficient à une matiere
+    @GetMapping("/matiere/coefficient")
+    public String addCoefficient(){
+        return "addCoefficient";
+    }
+
+    //inscrire un eleve
+    @GetMapping("/eleve/create")
+    public String inscrireEleve(){
+        return "addEleve";
+    }
+}
--- src/main/java/sen/school/primaire/controller/LoginController.java
+++ src/main/java/sen/school/primaire/controller/LoginController.java
@@ -1,13 +1,24 @@
 package sen.school.primaire.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@Slf4j
 public class LoginController {
 
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @PostMapping("/connexion")
+    public String verify(
+            @RequestParam(value = "login") String login,
+            @RequestParam(value = "password") String password){
+        log.info("======== Login ="+login);
+        log.info("======== Passsword ="+password);
+        return "admin";
     }
 }
