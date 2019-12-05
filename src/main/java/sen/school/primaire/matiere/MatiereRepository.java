aa4f2f51e956777d783cd00ab0788ada695879b2
MoiMemeMoussa <moimememoussa@users.noreply.github.com>
2019-12-05 09:22:21 +0100

    -Creation des fichiers MatiereApi MatiereRepositoryService MatiereRepository

---
+++ src/main/java/sen/school/primaire/matiere/MatiereRepository.java
@@ -0,0 +1,8 @@
+package sen.school.primaire.matiere;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatiereRepository extends JpaRepository<Matiere, Integer> {
+
+
+}
---
+++ src/main/java/sen/school/primaire/matiere/MatiereRepositoryService.java
@@ -0,0 +1,32 @@
+package sen.school.primaire.matiere;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Component
+public class MatiereRepositoryService {
+
+    @Autowired
+    MatiereRepository matiereRepository;
+
+    Matiere save(Matiere matiere) {
+        return matiereRepository.save(matiere);
+    }
+
+    Optional<Matiere> findById(Integer idMatiere) {
+        return matiereRepository.findById(idMatiere);
+    }
+
+    List<Matiere> findAll() {
+        return matiereRepository.findAll();
+    }
+
+    void delete(Matiere matiere) {
+        matiereRepository.delete(matiere);
+    }
+}
